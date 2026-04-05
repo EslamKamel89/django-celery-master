691cc42c6cfd534d4126c33f099efc57f159b1e4
EslamKamel89 <eslam_kamel_89@hotmail.com>
2026-04-05 09:06:37 +0200

    feat(celery): configure RabbitMQ broker with priority queue support and update worker setup

--- app/celery.py
+++ app/celery.py
@@ -2,6 +2,7 @@ import os
 
 from celery import Celery
 from django.conf import settings
+from kombu import Exchange, Queue
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 app = Celery("app")
@@ -10,13 +11,24 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 #     "cworker.tasks.task1": {"queue": "queue1"},
 #     "cworker.tasks.task2": {"queue": "queue2"},
 # }
-app.conf.broker_transport_options = {
-    "priority_steps": list(range(10)),
-    "sep": ":",
-    "queue_order_strategy": "priority",
-}
-app.conf.task_default_queue = "celery"
-app.conf.task_default_priority = 5
+# app.conf.broker_transport_options = {
+#     "priority_steps": list(range(10)),
+#     "sep": ":",
+#     "queue_order_strategy": "priority",
+# }
+# app.conf.task_default_queue = "celery"
 app.conf.worker_prefetch_multiplier = 1
-app.conf.task_default_rate_limit = "1/m"
+app.conf.worker_concurrency = 1
+# app.conf.task_default_rate_limit = "1/m"
+app.conf.task_acks_late = True
+app.conf.task_queues = [
+    Queue(
+        "tasks",
+        exchange=Exchange("tasks", type="direct", durable=True),
+        routing_key="tasks",
+        durable=True,
+        queue_arguments={"x-max-priority": 10},
+    )
+]
+app.conf.task_default_priority = 5
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
--- app/settings.py
+++ app/settings.py
@@ -128,5 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
-CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+# CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+# CELERY_RESULT_BACKEND = os.environ.get("CELERY_BACKEND", "redis://redis:6379/0")
+CELERY_BROKER_URL = os.environ.get(
+    "CELERY_BROKER", "amqp://user:password@rabbitmq:5672/"
+)
 CELERY_RESULT_BACKEND = os.environ.get("CELERY_BACKEND", "redis://redis:6379/0")
